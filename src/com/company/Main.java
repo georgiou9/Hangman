7eccac6315276a0f26b14edfacb56f185ee279b0
oldboy9 <afos3@hotmail.com>
2017-05-11 13:34:41 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Usage : java Hangman <answer>");
+            System.out.println("answer is required");
+            System.exit(1);
+        }
+
+        Game game = new Game(args[0]);
+        Prompter prompter = new Prompter(game);
+        prompter.displayProgress();
+        boolean isHit = prompter.promptForGuess();
+        while (game.getRemainingTries() > 0 && !game.isWon()) {
+            prompter.displayProgress();
+            prompter.promptForGuess();
+        }
+        prompter.displayOutcome();
+
+    }
+}
